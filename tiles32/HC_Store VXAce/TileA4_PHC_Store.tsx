e0ed64daa909f6e786edbb4f9e17e570a6ed53fe
hashinana <67550402+hashinana@users.noreply.github.com>
2021-12-26 17:39:20 +0100

    test+ export, copyright

---
+++ tiles32/HC_Store VXAce/TileA4_PHC_Store.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TileA4_PHC_Store" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <properties>
+  <property name="tilesetCopyright">This pack is Royalty Free and usable in any engine without limitation.
+ created by:  [VexedEnigma/PixelJustice] : [D Machin] </property>
+ </properties>
+ <image source="TileA4_PHC_Store.png" width="512" height="480"/>
+</tileset>
