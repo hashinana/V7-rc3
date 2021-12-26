45fa349e7a47d9d90eefd2144e81b94e2cd678d7
hashinana <67550402+hashinana@users.noreply.github.com>
2021-12-26 17:22:20 +0100

    test

---
+++ tiles32/HC_Store VXAce/PHC_StoreExteriorDAlt.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="PHC_StoreExteriorDAlt" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <properties>
+  <property name="tilesetCopyright">This pack is Royalty Free and usable in any engine without limitation.
+ created by:  [VexedEnigma/PixelJustice] : [D Machin] </property>
+ </properties>
+ <image source="PHC_StoreExteriorDAlt.png" width="512" height="512"/>
+</tileset>
---
+++ tiles32/HC_Store VXAce/TileA5_PHC_Store..tsx
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?><tileset version="1.4" tiledversion="1.4.3" name="TileA5_PHC_Store" tilewidth="32" tileheight="32" tilecount="128" columns="8"><properties><property name="tilesetCopyright">This pack is Royalty Free and usable in any engine without limitation.
+ created by:  [VexedEnigma/PixelJustice] : [D Machin] </property></properties><image source="TileA5_PHC_Store.png" width="256" height="512"/></tileset>
